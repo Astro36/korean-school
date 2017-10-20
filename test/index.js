75df7a6349c5f8d90682ab18959a46262ecc4cdf
Seungjae Park <astro.psj@gmail.com>
2017-10-20 16:20:46 +0900

    Add {getSchedule} method and change lots of APIs

--- test/index.js
+++ test/index.js
@@ -2,6 +2,9 @@ const { expect } = require('chai');
 const school = require('../lib');
 
 describe('school', () => {
+  const schoolData = school.find('경기도', '백석고');
+  const now = new Date();
+
   describe('.find(office, schoolName)', () => {
     context('when not present', () => {
       it('should return null', () => expect(school.find('해리포터', '호그와트')).to.be.null);
@@ -20,41 +23,56 @@ describe('school', () => {
     });
   });
 
-  describe('.neis', () => {
-    const schoolData = school.find('경기도', '백석고');
-    const now = new Date();
-
-    describe('.getMeals(school, date, callback)', () => {
-      context('when present', () => {
-        it('should return the meal info as an array', (done) => {
-          school.neis.getMeals(schoolData, now, (content) => {
-            expect(content).to.be.a('array');
-            done();
-          });
-        });
-      });
-    });
-
-    describe('.getMeal(school, date, callback)', () => {
-      context('when present', () => {
-        it('should return the meal info as an object', (done) => {
-          school.neis.getMeal(schoolData, now, (content) => {
-            expect(content).to.be.a('object');
-            done();
-          });
+  describe('.getInformation(school, callback)', () => {
+    context('when present', () => {
+      it('should return the school info as an object', (done) => {
+        school.getInformation(school.find('경기도', '백석고'), (content) => {
+          expect(content).to.be.a('object');
+          done();
         });
       });
     });
   });
 
-  describe('.schoolinfo', () => {
-    describe('.getSummary(school, callback)', () => {
-      context('when present', () => {
-        it('should return the school info as an object', (done) => {
-          school.schoolinfo.getSummary(school.find('경기도', '백석고'), (content) => {
-            expect(content).to.be.a('object');
-            done();
-          });
+  describe('.getMeal(school, date, callback)', () => {
+    context('when present', () => {
+      it('should return the school daily meal info as an object', (done) => {
+        school.getMeal(schoolData, now, (content) => {
+          expect(content).to.be.a('object');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('.getMeals(school, date, callback)', () => {
+    context('when present', () => {
+      it('should return the school monthly meal info as an array', (done) => {
+        school.getMeals(schoolData, now, (content) => {
+          expect(content).to.be.a('array');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('.getSchedule(school, grade, room, date, callback)', () => {
+    context('when present', () => {
+      it('should return the school daily schedule as an array', (done) => {
+        school.getSchedule(schoolData, 1, 1, now, (content) => {
+          expect(content).to.be.a('array');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('.getSchedules(school, grade, room, callback)', () => {
+    context('when present', () => {
+      it('should return the school daily schedule as an array', (done) => {
+        school.getSchedules(schoolData, 1, 1, (content) => {
+          expect(content).to.be.a('array');
+          done();
         });
       });
     });
