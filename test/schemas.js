2fd8640ad068e066f6068be3c694da98e70414af
Seungjae Park <astro.psj@gmail.com>
2017-11-22 15:35:44 +0900

    Add license notices on each source files and fix schema bug

--- test/schemas.js
+++ test/schemas.js
@@ -1,3 +1,19 @@
+/* KoreanSchool
+Copyright (C) 2017  Astro
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 const createNullableSchema = type => ({ oneOf: [type, { type: 'null' }] });
 
 const nullableStringSchema = createNullableSchema({ type: 'string' });
@@ -133,7 +149,7 @@ const schoolScheduleSchema = {
       teacher: nullableStringSchema,
       isChanged: 'boolean',
     },
-    required: ['breakfast', 'subjectOriginal', 'teacher', 'isChanged'],
+    required: ['subject', 'subjectOriginal', 'teacher', 'isChanged'],
   }),
 };
 const schoolScheduleArraySchema = {
